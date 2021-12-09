213a14ed2c5a4b9d7a67e8aaef991648069060cf
Username <avivfaraj4@gmail.com>
2021-12-08 19:58:24 -0500

    new_accounts method is missing

--- src/Person.py
+++ src/Person.py
@@ -1,20 +1,53 @@
 
 class Person(object):
-	# def setID(self, id_ = ""):
-	# 	if id_:
-	# 		self.id = id_
-	# 	else:
-	# 		self.id = 12222
+
+	ID : int
+	name : str
+	accounts : list
 
 	def __init__(self, id_ = None, name = "", accounts = []):
-		self.setID(id_)
+		self.set_ID(id_)
+		self.set_name(name)
+		self.set_accounts(accounts)
 
-	def setID(self, id_ = ""):
+	def set_ID(self, id_ = ""):
 		if id_:
-			self.id = id_
+			self.ID = id_
 		else:
-			self.id = 12222
+			raise ValueError("ID must be positive")
+
+	def set_name(self, name = ""):
+		if isinstance(name, str):
+			if name:
+				self.name = name
+			else:
+				raise ValueError("Name was empty")
+		else:
+			raise TypeError("Name must be a string")
+
+	def set_accounts(self, accounts = []):
+		self.accounts = accounts
 
 
-a = Person()
-print(a.id)
+	def get_ID(self):
+		return self.ID
+
+	def get_name(self):
+		return self.name
+
+	def get_accounts(self):
+		return self.accounts
+
+	def new_account(self):
+		# To Do
+		pass
+
+
+	def __str__(self):
+		return ("ID: " + str(self.get_ID()) +
+				"\nName: " + self.get_name())
+
+
+
+a = Person(id_ = 1, name = "Aviv")
+print(a)
