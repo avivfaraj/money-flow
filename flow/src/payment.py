d39004de5ceb768e3d2f8b01dbad07763c516db3
avivfaraj <avivfaraj4@gmail.com>
2022-07-14 21:11:10 -0500

    Fixed flake8 errors

--- flow/src/payment.py
+++ flow/src/payment.py
@@ -1,51 +1,51 @@
 from abc import ABC, abstractmethod
 
+
 class Payment(ABC):
 
-	ID : int
-	company : str
-	account_num : int
+    ID: int
+    company: str
+    account_num: int
 
-	@abstractmethod
-	def __init__(self):
-		pass
-		
-	@property
-	def ID(self):
-		return self._ID
+    @abstractmethod
+    def __init__(self):
+        pass
 
-	@ID.setter
-	def ID(self, value = -1):
-		if value:
-			if isinstance(value, int) and value > 0 and len(str(value)) == 4:
-				self._ID = value
-			else:
-				raise ValueError("ID is not valid")
-		else:
-			raise NotImplementedError("ID is required")
+    @property
+    def ID(self):
+        return self._ID
 
-	@property
-	def company(self):
-		return self._company
+    @ID.setter
+    def ID(self, value = -1):
+        if value:
+            if isinstance(value, int) and value > 0 and len(str(value)) == 4:
+                self._ID = value
+            else:
+                raise ValueError("ID is not valid")
+        else:
+            raise NotImplementedError("ID is required")
 
-	@company.setter
-	def company(self, value = "" ):
-		if value:
-			self._company = value
-		else:
-			raise ValueError("Payment Company is missing!")
+    @property
+    def company(self):
+        return self._company
 
-	@property
-	def account_num(self):
-		return self._account_num
+    @company.setter
+    def company(self, value = ""):
+        if value:
+            self._company = value
+        else:
+            raise ValueError("Payment Company is missing!")
 
-	@account_num.setter
-	def account_num(self, value = None):
-		if value:
-			if not isinstance(value,int):
-				raise TypeError("Account Number must be an integer")
-			else:
-				self._account_num = value
-		else:
-			raise ValueError("Account Number is None. It must be an integer")
+    @property
+    def account_num(self):
+        return self._account_num
 
+    @account_num.setter
+    def account_num(self, value = None):
+        if value:
+            if not isinstance(value, int):
+                raise TypeError("Account Number must be an integer")
+            else:
+                self._account_num = value
+        else:
+            raise ValueError("Account Number is None. It must be an integer")
